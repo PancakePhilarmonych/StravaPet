5cd344db7925eae6200ba23d0dc84019d5b73439
PancakePhilarmonych <rikko.alexandr@gmail.com>
2025-11-23 20:56:25 +0400

    feat: update signin logic

--- frontend/src/layout/AppView.vue
+++ frontend/src/layout/AppView.vue
@@ -5,7 +5,18 @@
   <nav>
     <RouterLink to="/home">Главная</RouterLink>
     <RouterLink to="/shop">Магазин</RouterLink>
+    <button @click="logout">Выход</button>
   </nav>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+function logout() {
+  localStorage.removeItem('session_token');
+  localStorage.removeItem('user_id');
+  router.push('/auth');
+}
+</script>
 
--- frontend/src/pages/AuthPage.vue
+++ frontend/src/pages/AuthPage.vue
@@ -1,31 +1,46 @@
 <template>
-  AUTH LAYOUT
-  ЛОГИНЬСФ
-  <button @click="auth">AUTH</button>
+  <div v-if="isLoading">Загрузка...</div>
+  <div v-else>
+    AUTH LAYOUT
+    ЛОГИНЬСФ
+    <button @click="auth">AUTH</button>
+  </div>
 </template>
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { useRoute } from 'vue-router';
+import { onMounted, ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute();
+const router = useRouter();
+const isLoading = ref(false);
 
 onMounted(async () => {
   const code = route.query.code;
 
   if (code) {
-    const response = await fetch(`http://${window.location.hostname}:${import.meta.env.VITE_BACKEND_PORT}/auth/strava`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ code  }),
-    });
+    isLoading.value = true;
 
-    const data = await response.json();
+    try {
+      const response = await fetch(`http://${window.location.hostname}:${import.meta.env.VITE_BACKEND_PORT}/auth/strava`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ code  }),
+      });
 
-    if (data.session_token && data.user_id) {
-      localStorage.setItem('session_token', data.session_token);
-      localStorage.setItem('user_id', data.user_id);
+      const data = await response.json();
+
+      if (data.session_token && data.user_id) {
+        localStorage.setItem('session_token', data.session_token);
+        localStorage.setItem('user_id', data.user_id);
+        await router.push('/');
+
+      } else {
+        isLoading.value = false;
+      }
+    } catch {
+      isLoading.value = false;
     }
   }
 });
